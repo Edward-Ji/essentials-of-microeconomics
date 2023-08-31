320658cd4e519430d2ba5d32ca97f35c8a12a01f
Edward Ji <jiziao6@gmail.com>
2023-08-31 22:41:41 +1000

    feat: add production and costs tab
    Add marginal product section inside the tab.

--- essentials_of_microeconomics/app.py
+++ essentials_of_microeconomics/app.py
@@ -1,15 +1,30 @@
 from shiny import App, ui
+from common import mathjax_script
 from tp import tp_ui, tp_server
+from pc import pc_ui, pc_server
 
 app_ui = ui.page_fluid(
+    ui.head_content(
+        ui.tags.style("""div.tab-pane {
+            max-width: 800px;
+            margin: auto;
+        }"""),
+        ui.tags.script(
+            src="https://mathjax.rstudio.com/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"
+        ),
+        mathjax_script
+    ),
     ui.navset_tab(
-        tp_ui("tp")
+        tp_ui("tp"),
+        pc_ui("pc"),
+        selected="Production and costs"
     )
 )
 
 
 def server(input, output, session):
     tp_server("tp")
+    pc_server("pc")
 
 
 app = App(app_ui, server)
