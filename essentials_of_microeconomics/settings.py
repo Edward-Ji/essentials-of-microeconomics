0932f71d40beea3e5ee8179478ace32aed5a5902
Edward Ji <jiziao6@gmail.com>
2023-09-24 12:14:25 +1000

    feat: add input storage settings
    Users can now manually save and clear input storage, also toggle save
    on input changes.

--- essentials_of_microeconomics/settings.py
+++ essentials_of_microeconomics/settings.py
@@ -18,11 +18,31 @@ def settings_ui():
                        data_bs_dismiss="modal", aria_label="Close settings")
         )
     body = (
+        ui.h2("Evaluation"),
         ui.input_select("approx", "Numerical evaluation of rationals:",
                         [e.value for e in Approx]),
         ui.panel_conditional(
             f"input.approx !== '{Approx.HIDE.value}'",
-            ui.input_slider("perc", "Binary percision:", 0, 100, 15))
+            ui.input_slider("perc", "Binary percision:", 0, 100, 15)),
+        ui.h2("Input storage"),
+        ui.p("""Automatically save inputs when they change or manually save and
+             clear inputs. Clear and refresh to reset all inputs to
+             default."""),
+        ui.div(
+            ui.tags.button("Auto",
+                           id="input-auto-btn",
+                           class_="btn btn-outline-secondary",
+                           data_bs_toggle="button"),
+            ui.tags.button("Save",
+                           id="input-save-btn",
+                           class_="btn btn-outline-secondary"),
+            ui.tags.button("Clear",
+                           id="input-clear-btn",
+                           class_="btn btn-outline-danger"),
+            class_="btn-group",
+            role="group",
+            aria_label="Input storage button group")
+
         )
     return ui.div(
         ui.div(
--- essentials_of_microeconomics/www/main.js
+++ essentials_of_microeconomics/www/main.js
@@ -1,18 +1,103 @@
-$(window).bind("load", function() {
-    for (id in localStorage) {
+/* Settings and input storage */
+function getStorageJSON(key) {
+    if (localStorage[key] === undefined) return {};
+    try {
+        return JSON.parse(localStorage[key]);
+    } catch (e) {
+        return {};
+    }
+}
+
+function setStorageJSON(key, id, value) {
+    var obj = getStorageJSON(key);
+    obj[id] = value;
+    localStorage[key] = JSON.stringify(obj);
+}
+
+function setValues(dict) {
+    for (id in dict) {
         var element = $("#" + id);
         if (element.hasClass("js-range-slider")) {
             var data = element.data("ionRangeSlider");
-            data.update({from: localStorage[id]});
+            data.update({from: dict[id]});
         } else {
-            element.val(localStorage[id]);
+            element.val(dict[id]);
         }
-        Shiny.setInputValue(id, localStorage[id]);
+        Shiny.setInputValue(id, dict[id]);
+    }
+}
+
+function loadSettings() {
+    var settings = getStorageJSON("settings");
+    setValues(settings);
+}
+
+function saveSettingsBind() {
+    $("[class*='shiny-bound-input'][id|='settings']")
+        .on("shiny:inputchanged", function(){
+            setStorageJSON("settings", $(this).attr("id"), $(this).val());
+        });
+}
+
+function loadInput() {
+    var input = getStorageJSON("input");
+    setValues(input);
+}
+
+function saveInput() {
+    var input = {};
+    $("[class*='shiny-bound-input']")
+        .each(function(index) {
+            var id = $(this).attr("id");
+            var value = Shiny.shinyapp.$inputValues[id];
+            input[id] = value;
+        });
+    localStorage.setItem("input", JSON.stringify(input));
+}
+
+function saveInputEvent(event) {
+    setStorageJSON("input", $(this).attr("id"), $(this).val());
+}
+
+function saveInputBind() {
+    $("[class*='shiny-bound-input']")
+        .not("[id|='settings']")
+        .on("shiny:inputchanged", saveInputEvent);
+}
+
+function saveInputUnbind() {
+    $("[class*='shiny-bound-input']")
+        .not("[id|='settings']")
+        .off("shiny:inputchanged", saveInputEvent);
+}
+
+function clearInput() {
+    localStorage.removeItem("input");
+}
+
+$(document).on("shiny:connected", function() {
+    loadSettings();
+    loadInput();
+    saveSettingsBind();
+    if (localStorage.getItem("input-auto") === "true") {
+        saveInputBind();
+        bootstrap.Button.getOrCreateInstance($("#input-auto-btn")).toggle();
     }
 })
 
-$(document).on("change", "[class*='shiny-bound-input']", function() {
-    localStorage.setItem($(this).attr("id"), $(this).val());
+$(function() {
+    $("#input-save-btn").on("click", saveInput);
+    $("#input-clear-btn").on("click", clearInput);
+    $("#input-auto-btn").on("click", function(event) {
+        if ($(this).hasClass("active")) {
+            saveInputBind();
+            localStorage.setItem("input-auto", "true");
+        } else {
+            saveInputUnbind();
+            localStorage.setItem("input-auto", "false");
+        }
+    });
+})
 
 /* MathJax */
 $("#MathJax-script").on("load", function() {
